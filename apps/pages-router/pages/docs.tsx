184ebfefeb1c0beeccb3c6ea557f56c8bad97959
Guido linux del <iguidoo@outlook.com>
2024-11-26 14:30:59 +0100

    hjk

---
+++ apps/pages-router/pages/docs.tsx
@@ -0,0 +1,11 @@
+// pages/docs.tsx
+const Docs = () => {
+  return (
+    <div>
+      <h1>Documentation</h1>
+      <p>Welcome to the documentation page.</p>
+    </div>
+  )
+}
+
+export default Docs
